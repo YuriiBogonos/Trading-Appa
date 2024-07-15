f54bb840e16395ad2cac107db06cda8ba17c983a
YuriiBogonos <ubogonos2003@gmail.com>
2024-07-15 17:24:20 +0300

    feat: Update tradeApi base URL and endpoint paths

--- src/store/features/trades/tradesSlice.ts
+++ src/store/features/trades/tradesSlice.ts
@@ -3,20 +3,22 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const tradeApi = createApi({
   reducerPath: 'tradeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://34.253.69.53:8000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://201.222.55.50:8000/trades/' }),
   endpoints: (builder) => ({
     openTrade: builder.mutation<TradeResponse, TradeRequest>({
       query: (tradeData) => ({
-        url: 'initiate-trade/',
+        url: 'initiate-position',
         method: 'POST',
         body: tradeData,
       }),
     }),
     fetchTradeStatus: builder.query<TradeStatus, string>({
+      //TODO: Replase sessionId with tradeId
       query: (sessionId) => ({
-        url: `check-trade-status/${sessionId}`,
+        url: `positions/${sessionId}`,
         method: 'GET',
       }),
+      transformResponse: (response: TradeStatus[]) => response[0],
     }),
   }),
 });
